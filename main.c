5906f73f91261b4503f02c1ac4238d3eb3e268f3
audreytilanov <57605001+audreytilanov@users.noreply.github.com>
2021-04-29 19:54:59 +0800

    kedua
    kedua

--- main.c
+++ main.c
@@ -9,57 +9,57 @@ int main(){
 		scanf("%d", &arr);
 	}
 }
-//int status;
-//	int jabatan;
-//	int upah;
-//	int statusMhs;
-//	int statusPnt;
-//	
-//	printf("Masukkan status\n1.Mahasiswa\n2.Non Mahasiswa\n");
-//	scanf("%d",&status);
-//	
-//	printf("Pilih Salah Satu Komen Diatas");
-//	switch(status){
-//		case 1 :{
-//			statusMhs = 1;
-//			break;
-//		}
-//		case 2:{
-//			statusMhs = 2;
-//			break;
-//		}
-//		default:{
-//			printf("Inputan Status Salah\n");
-//			break;
-//		}
-//	}
-//	
-//	printf("Masukkan Jabatan\n1.Panitia\n2.Non Panitia\n");
-//	scanf("%d",&jabatan);
-//	
-//	printf("Pilih Salah Satu Komen Diatas");
-//	switch(jabatan){
-//		case 1 :{
-//			statusPnt = 1;
-//			break;
-//		}
-//		case 2:{
-//			statusPnt = 2;
-//			break;
-//		}
-//		default:{
-//			printf("Inputan Jabatan Salah\n");
-//			break;
-//		}
-//	}
-//	if(statusMhs == 1 && statusPnt == 1){
-//		upah = 100000;
-//	}else if(statusMhs == 1 && statusPnt == 2){
-//		upah = 20000;
-//	}else if(statusMhs == 2 && statusPnt == 1){
-//		upah = 70000;
-//    }else if(statusMhs == 2 && statusPnt == 2){
-//    	upah = 5000;
-//    }
-//    printf("%d", upah);
-//	return 0;
+int status;
+	int jabatan;
+	int upah;
+	int statusMhs;
+	int statusPnt;
+	
+	printf("Masukkan status\n1.Mahasiswa\n2.Non Mahasiswa\n");
+	scanf("%d",&status);
+	
+	printf("Pilih Salah Satu Komen Diatas");
+	switch(status){
+		case 1 :{
+			statusMhs = 1;
+			break;
+		}
+		case 2:{
+			statusMhs = 2;
+			break;
+		}
+		default:{
+			printf("Inputan Status Salah\n");
+			break;
+		}
+	}
+	
+	printf("Masukkan Jabatan\n1.Panitia\n2.Non Panitia\n");
+	scanf("%d",&jabatan);
+	
+	printf("Pilih Salah Satu Komen Diatas");
+	switch(jabatan){
+		case 1 :{
+			statusPnt = 1;
+			break;
+		}
+		case 2:{
+			statusPnt = 2;
+			break;
+		}
+		default:{
+			printf("Inputan Jabatan Salah\n");
+			break;
+		}
+	}
+	if(statusMhs == 1 && statusPnt == 1){
+		upah = 100000;
+	}else if(statusMhs == 1 && statusPnt == 2){
+		upah = 20000;
+	}else if(statusMhs == 2 && statusPnt == 1){
+		upah = 70000;
+    }else if(statusMhs == 2 && statusPnt == 2){
+    	upah = 5000;
+    }
+    printf("%d", upah);
+	return 0;
